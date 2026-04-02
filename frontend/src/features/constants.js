4cdbda2be1d5033926d67d2d1e622286e633dde2
Kashika Gupta <93068969+kashikaga@users.noreply.github.com>
2026-04-03 00:52:15 +0530

    Update API_URL to new backend URL

--- frontend/src/features/constants.js
+++ frontend/src/features/constants.js
@@ -1,4 +1,2 @@
 export const API_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8080'
-    : 'https://fintrak-frontend.onrender.com'
+"https://fintrak-backend-l057.onrender.com"
